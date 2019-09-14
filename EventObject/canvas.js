10ab60b2ebbe13803ab4984baadb2253f995a55c
Zurol <zurolk@gmail.com>
2019-09-13 23:22:16 -0500

    Colisión básica por distancia entre núcleos de los objetos

--- EventObject/canvas.js
+++ EventObject/canvas.js
@@ -5,8 +5,8 @@ canvas.width = window.innerWidth-2;
 canvas.height = window.innerHeight-2;
 
 var mouse = {
-	x : innerWidth / 2,
-	y : innerHeight / 2
+	x : 10,
+	y : 10
 }
 
 var colorArray = [
@@ -66,78 +66,75 @@ function randomIntFromRange(min,max){
 }
 
 /**
- * [Objeto esfera]
- * @param {float} x      [Posición X de la esfera]
- * @param {float} y      [Posición Y de la esfera]
- * @param {float} dx     [Velocidad de desplazamiento Dx]
- * @param {float} dy     [Velocidad de desplazamiento Dy]
- * @param {  int} radius [Tamaño del radio la esfera]
- * @param {sting} color  [Color del llenado]
+ * Función para generar la distancia entre 2 puntos (x,y)
+ * @param  {float} x1 [Posicion del origen "X" del punto_1]
+ * @param  {float} y1 [Posicion del origen "Y" del punto_1]
+ * @param  {float} x2 [Posicion del origen "X" del punto_2]
+ * @param  {float} y2 [Posicion del origen "Y" del punto_2]
+ * @return {float}    [Distancia entre 2 puntos - Norma Vectorial]
  */
-function Ball(x, y, dx, dy, radius, color){
+function getDistance(x1, y1, x2, y2){
+	let xDistance = x2 - x1;
+	let yDistance = y2 - y1;
+
+	return Math.sqrt( Math.pow(xDistance, 2) + Math.pow(yDistance, 2) );
+}
+
+
+/**
+ * Objeto -círculo- para la demostración de funcionalidad.
+ * @param {int} x      	 [Coordenada X del origen del círculo]
+ * @param {int} y      	 [Coordenada Y del origen del círculo]
+ * @param {int} radius 	 [Dimensión del Radio del círculo]
+ * @param {string} color [Color de relleno de la figura]
+ */
+function Circle(x, y, radius, color){
 	this.x = x;
 	this.y = y;
-	this.dx = dx;
-	this.dy = dy;
 	this.radius = radius;
 	this.color = color;
 
 	this.update = function(){
-		if (this.y + this.radius + this.dy > canvas.height) {
-			this.dy = -this.dy * friction;
-		} else{
-			this.dy += gravity;
-		}
-
-		if (this.x + this.radius +this. dx > canvas.width || this.x - this.radius < 0) {
-			this.dx = -this.dx;
-		}
-
-		this.x += this.dx;
-		this.y += this.dy;
 		this.draw();
-	};
+	}
 
 	this.draw = function(){
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
 		ctx.fillStyle = this.color;
-
 		ctx.fill();
-		ctx.stroke();
 		ctx.closePath();
 	}
 }
 
-
-
-var ball;
-var ballArray = [];
+let circle1;
+let circle2;
 
 /**
- * [Inicializador del dibujado + Inicialización de esferas con valores random]
+ * [Inicializador del dibujado]
  * @return {} [Sin retorno]
  */
 function init(){
-	ballArray = [];
-	for (var i = 0; i < 400; i++) {
-		var radius = randomIntFromRange(8, 30);
-		var x = randomIntFromRange(radius, canvas.width - radius);
-		var y = randomIntFromRange(0, canvas.height - radius);
-		var dx = randomIntFromRange(-2, 2);
-		var dy = randomIntFromRange(-2, 2);
-		var color = randomColorPalette();
-		ballArray.push(new Ball(x, y, dx, dy, radius, color));
-	}
+	circle1 = new Circle(300, 300, 100, 'black');
+	circle2 = new Circle(undefined, undefined, 30, 'red');
 }
 
 function animate(){
 	requestAnimationFrame(animate);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	for (var i = 0; i < ballArray.length; i++) {
-		ballArray[i].update();
+	circle1.update();
+	circle2.x = mouse.x;
+	circle2.y = mouse.y;
+	circle2.update();
+
+	if (getDistance(circle1.x, circle1.y, circle2.x, circle2.y) < circle1.radius + circle2.radius) {
+		circle1.color = 'red';
+	} else{
+		circle1.color = 'black';
 	};
+
+	console.log(getDistance(circle1.x, circle1.y, circle2.x, circle2.y));
 }
 
 init();
