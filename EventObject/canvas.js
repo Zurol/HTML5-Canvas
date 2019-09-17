1efea6dc76e3f793e362b47b95255cd6a14b0990
Zurol <zurolk@gmail.com>
2019-09-16 23:25:43 -0500

    Funciones de rotación y resolución de colisión directas del curso

--- EventObject/canvas.js
+++ EventObject/canvas.js
@@ -86,6 +86,68 @@ function getDistance(x1, y1, x2, y2){
 
 
 /**
+ * Define un sistema de ejes (referencias) variable para las particulas, permitiendo mantener el desplazamiento sobre un eje x y y con un giro, en vez de ángulos complejos para cada partícula. 
+ * Extracción directa del repositorio de utilidades del curso.
+ * @param  {[double]} velocity [Velocidad individual de la partícula]
+ * @param  {[double]} angle    [Ángulo de colisión entre 2 objetos - en radianes]
+ * @return {[double]}          [El sistema de velocidades X y Y después de la rotación del sistema]
+ */
+function rotate (velocity, angle) {
+	const rotatedVelocities = {
+		x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+		y: velocity.x * Math.sin(angle) - velocity.y * Math.cos(angle),
+	}
+	return rotatedVelocities;
+}
+
+
+/**
+ * Cambio de velocidades X y Y después de la colisión entre 2 partículas.
+ * @param  {[Particle]} particle      [Partícula con Coordinadas(X,Y) y Velocidad(X,Y)]
+ * @param  {[Particle]} otherParticle [Partícula con Coordinadas(X,Y) y Velocidad(X,Y)]
+ * @return {[Null] 					  [Sin Retorno]
+ */
+function resolveCollision(particle, otherParticle) {
+    const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+    const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+
+    const xDist = otherParticle.x - particle.x;
+    const yDist = otherParticle.y - particle.y;
+
+    // Prevent accidental overlap of particles
+    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+
+        // Grab angle between the two colliding particles
+        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+
+        // Store mass in var for better readability in collision equation
+        const m1 = particle.mass;
+        const m2 = otherParticle.mass;
+
+        // Velocity before equation
+        const u1 = rotate(particle.velocity, angle);
+        const u2 = rotate(otherParticle.velocity, angle);
+
+        // Velocity after 1d collision equation
+        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
+        const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+
+        // Final velocity after rotating axis back to original location
+        const vFinal1 = rotate(v1, -angle);
+        const vFinal2 = rotate(v2, -angle);
+
+        // Swap particle velocities for realistic bounce effect
+        particle.velocity.x = vFinal1.x;
+        particle.velocity.y = vFinal1.y;
+
+        otherParticle.velocity.x = vFinal2.x;
+        otherParticle.velocity.y = vFinal2.y;
+    }
+}
+
+
+
+/**
  * Objeto -círculo- para la demostración de funcionalidad.
  * @param {int} x      	 [Coordenada X del origen del círculo]
  * @param {int} y      	 [Coordenada Y del origen del círculo]
@@ -140,7 +202,7 @@ let particles;
 
 /**
  * Dibujado de 2 círculos para evaluar la distancia entre ambos.
- * @return {} [Sin retorno]
+ * @return {[Null] [Sin retorno]
  */
 function init(){
 	particles = [];
